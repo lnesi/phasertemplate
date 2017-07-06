5738a7fdd627b63cf8ca76474776b93c73350ab4
Luis Nesi <luis.nesi@two-uk.com>
2017-07-06 12:16:27 +0100

    Add sass Support

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-var gulp 		= require('gulp');
-var webserver 	= require('gulp-webserver');
-var include 	= require("gulp-include");
-var htmlmin 	= require('gulp-htmlmin');
-var watch 		= require('gulp-watch');
-var ts 			= require('gulp-typescript');
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var include = require("gulp-include");
+var htmlmin = require('gulp-htmlmin');
+var watch = require('gulp-watch');
+var ts = require('gulp-typescript');
+var sass = require('gulp-sass');
 
-// gulp.task("scripts", function() {
-// 	gulp.src("src/js/main.js")
-// 	.pipe(include())
-// 	.pipe(gulp.dest("build/js"));
-// });	
 
-gulp.task("typescript",function(){
-	return gulp.src('src/ts/*.ts')
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task("typescript", function() {
+    return gulp.src('src/ts/*.ts')
         .pipe(ts({
             noImplicitAny: true,
             outFile: 'main.js'
@@ -21,9 +23,9 @@ gulp.task("typescript",function(){
 });
 
 
-gulp.task("dependencies",function(){
-	gulp.src("node_modules/phaser/build/phaser.min.js")
-	.pipe(gulp.dest("build/js/vendor"));
+gulp.task("dependencies", function() {
+    gulp.src("node_modules/phaser/build/phaser.min.js")
+        .pipe(gulp.dest("build/js/vendor"));
 })
 
 gulp.task('webserver', function() {
@@ -36,19 +38,18 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('minify-html', function() {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('build'));
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('watch_src', function () {
+gulp.task('watch_src', function() {
     // Endless stream mode 
-    return watch('src/**/*', function(){
-    	gulp.start('build_src');
+    return watch('src/**/*', function() {
+        gulp.start('build_src');
     });
 });
 
-gulp.task('watch',['dependencies','watch_src']);
-gulp.task('build',['dependencies','typescript','minify-html']);
-gulp.task('build_src',['typescript','minify-html']);
-
+gulp.task('watch', ['dependencies', 'watch_src']);
+gulp.task('build', ['dependencies', 'build_src']);
+gulp.task('build_src', ['typescript', 'minify-html', 'sass']);
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,14 @@
 class Game{
-	
+	engine: Object;
+	constructor(){
+		var w=window.innerWidth*window.devicePixelRatio;
+		var h=window.innerHeight*window.devicePixelRatio;
+		this.engine=new Phaser.Game(w,h, Phaser.CANVAS, '', { preload: this.preload, create: this.created });
+	}
+	preload(){
+		console.log("PRELOAD");
+	}
+	created(){
+		console.log("CREATED");
+	}
 }
